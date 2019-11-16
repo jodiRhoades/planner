1da28069195578205f1299485734191e681ca761
Jodi Rhoades <jo_kawgirl@yahoo.com>
2019-11-16 18:28:51 -0500

    added files

---
+++ Assets/dayplanner.js
@@ -0,0 +1,21 @@
+// install moments code for the Day, MOnth and Date  in Jumbo
+var m = moment.parseZone();
+    $("#currentDay").text(m.format("dddd, MMMM Do"));
+
+
+    
+    
+// create  a table is Jquery with 3 columns time/event/save button
+    //column one with hours
+    //column two
+        // make time that is past gray
+        //time that is current red
+        //time coming up green
+        //border highlight on clicked time slot
+    //column three
+        //save tabs 
+        //blue, with icon, rounded on one side
+        //functionable
+
+    
+
