18311832769f2651b7ae4055993f8974098d4c54
Jodi Rhoades <jo_kawgirl@yahoo.com>
2019-11-17 18:20:10 -0500

    more code for local storage and color coding

--- dayplanner.js
+++ dayplanner.js
@@ -4,7 +4,10 @@ $("#currentDay").text(m.format("dddd, MMMM Do"));
 
 //hour blocks are color coded for current, past and future     
 var currentHour = new Date().getHours();
+
 $("currentHour").css("background-color", "red");
+console.log(currentHour);
+
 
 //Current hour in military time
 // use the above hour to know what row to change the color
@@ -12,22 +15,23 @@ $("currentHour").css("background-color", "red");
 for (var i = currentHour + 1; i < 18; i++) {
     $('#time-' + i).removeClass('lightgray')
     $('#time-' + i).addClass('bg-success')
-    //??not sure how well this functions since I'm not in the actual working hours of the planner
+    //not functioning.... not sure how to fix it
+}
+
+getSavedNotes();
+console.log("working")
+$(".saveBtn").click(function(){
+    console.log("click")
+    var time = $(this).siblings("div.hour").text();
+    var input = $(this).siblings("textarea.time-block").val();
+    localStorage.setItem(time, input);
+})
+
+function getSavedNotes() {
+    $("#1PM").val(localStorage.getItem("1PM"));
+
+    // $("time-9").val(localStorage.getItem("time-9"));
 
 }
-//on clicking the save button we save the new event to local storage
-var newEvent=[{"time-9":""}, {"time-10":""}, {"time-11":""}, {"time-12":""}, {"time-1":""}, {"time-2":""}, {"time-3":""}, {"time-4":""}, {"time-5":""}]
-$(".saveBtn").click(function(){
-   localStorage.setItem("newEvent", JSON.stringify(newEvent));
-   $("newEvent").text("newEvent");
-   //??feel like I need to ad JSONparse
-   //??Can't remembr how to "append" or is it "text"
-   //??I still need to get a value... but not sure where to put it or how. 
-   //??I can type "dentist" in textarea and I'll get a key of "newEvent but the Vaule is screwy
-   
-});    
-
-
-
 
 
