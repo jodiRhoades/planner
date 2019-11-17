30e37e85e219dc1af3e9812a0cdcd67f22a71ec5
Jodi Rhoades <jo_kawgirl@yahoo.com>
2019-11-16 22:31:14 -0500

    onclick, css, attempt at local storage

--- dayplanner.js
+++ dayplanner.js
@@ -1,21 +1,33 @@
-// install moments code for the Day, MOnth and Date  in Jumbo
+// install moments code for the Day, Month and Date  in Jumbo
 var m = moment.parseZone();
-    $("#currentDay").text(m.format("dddd, MMMM Do"));
+$("#currentDay").text(m.format("dddd, MMMM Do"));
+
+//hour blocks are color coded for current, past and future     
+var currentHour = new Date().getHours();
+$("currentHour").css("background-color", "red");
+
+//Current hour in military time
+// use the above hour to know what row to change the color
+// create a for loop 
+for (var i = currentHour + 1; i < 18; i++) {
+    $('#time-' + i).removeClass('lightgray')
+    $('#time-' + i).addClass('bg-success')
+    //??not sure how well this functions since I'm not in the actual working hours of the planner
+
+}
+//on clicking the save button we save the new event to local storage
+var newEvent=
+$(".saveBtn").click(function(){
+   localStorage.setItem("newEvent", JSON.stringify(newEvent));
+   $("newEvent").text("newEvent");
+   //??feel like I need to ad JSONparse
+   //??Can't remembr how to "append" or is it "text"
+   //??I still need to get a value... but not sure where to put it or how. 
+   //??I can type "dentist" in textarea and I'll get a key of "newEvent but the Vaule is screwy
+   
+});    
 
 
-    
-    
-// create  a table is Jquery with 3 columns time/event/save button
-    //column one with hours
-    //column two
-        // make time that is past gray
-        //time that is current red
-        //time coming up green
-        //border highlight on clicked time slot
-    //column three
-        //save tabs 
-        //blue, with icon, rounded on one side
-        //functionable
 
-    
+
 
