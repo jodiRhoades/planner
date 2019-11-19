cb8ec6bb99950190f5d75dd1cffd8e0b356a57a1
Jodi Rhoades <jo_kawgirl@yahoo.com>
2019-11-18 19:37:21 -0500

    fixed a bunch of issues that popped upo with other fixes and added read.me

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# planner
+# Day Planner
+
+This is a simple day planner for a 9-5pm work day. You'll be able to put your appointments in and save them for the day so that you can effectively manage your day better.
+![dayplanner demo](.)
+
+## Business Context
+
+Poor time management can result in missed meetings and deadlines or create the appearance of unprofessionalism. A daily planner allows employees to see their day at a glance, schedule time effectively, and improve productivity. 
+
+### Credits
+
+Several team players helped direct me when I was off base.  Crredits go to :  Max Allee, Kaighan Finely, Leah Barnes, Jim Gray, Lara ELler, Ryan Diaz, Christian Saxton, Rodney Brenes and Patrick Dorgu.
+
--- dayplanner.js
+++ dayplanner.js
@@ -4,43 +4,64 @@ $("#currentDay").text(m.format("dddd, MMMM Do"));
 
 //hour blocks are color coded for current, past and future     
 var currentHour = new Date().getHours();
+currentHour= 13
 
-$("currentHour").css("background-color", "red");
-console.log(currentHour);
-
+$('#time'+currentHour).addClass('bg-danger');
 
 //Current hour in military time
 // use the above hour to know what row to change the color
-// create a for loop 
-for (var i = currentHour + 1; i < 18; i++) {
-    $('#time-' + i).removeClass('lightgray')
-    $('#time-' + i).addClass('bg-success')
-    //not functioning.... not sure how to fix it
+// create a for loop to go through as the hour changes
+for(var i = currentHour + 1; i < 18; i++) {
+    $('#time' + i).removeClass('lightgray')
+    $('#time' + i).addClass('bg-success')    
+}
+
+for  (var j=9; j<18; j++){
+    getSavedNotes(j)
+}
+
+for  (var j=10; j<18; j++){
+    getSavedNotes(j)
+}
+
+for  (var j=11; j<18; j++){
+    getSavedNotes(j)
+}
+
+for  (var j=12; j<18; j++){
+    getSavedNotes(j)
+}
+
+for  (var j=13; j<18; j++){
+    getSavedNotes(j)
+}
+
+for  (var j=14; j<18; j++){
+    getSavedNotes(j)
+}
+
+for  (var j=15; j<18; j++){
+    getSavedNotes(j)
+}
+
+for  (var j=16; j<18; j++){
+    getSavedNotes(j)
+}
+
+for  (var j=17; j<18; j++){
+    getSavedNotes(j)
 }
 
 
-console.log("working")
 
-function grabData(id) {
-    console.log("The ID is: "+ id);
-    var input= $("#"+ id).val();
-    console.log(input);
-    localStorage.setItem(id, input);
-    getSavedNotes(); 
-}
+$('.btn').click(function() {
+    var hour= $(this).val()
+    localStorage.setItem(hour, $('#time'+hour).val())    
+})
+
 function getSavedNotes(id) {
-    $("#"+ id).val(localStorage.getItem(id));
+    $("#time"+id).val(localStorage.getItem(id));
     console.log(localStorage.getItem(id));    
 }
 
-getSavedNotes("9AM");
-getSavedNotes("10AM");
-getSavedNotes("11AM");
-getSavedNotes("12PM");
-getSavedNotes("1PM");
-getSavedNotes("2PM");
-getSavedNotes("3PM");
-getSavedNotes("4PM");
-getSavedNotes("5PM");
 
-//i can get a key and a value in application but it doesn't stay afer being refreshed.
